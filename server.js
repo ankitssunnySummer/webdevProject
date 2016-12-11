568cac21270ed4c1b182293bc4246bb7d391be39
ankitssunny <sharma.anki@husky.neu.edu>
2016-12-11 17:36:07 -0500

    asdasdasdadsasdasda

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
+var cookieParser = require('cookie-parser');
+var session      = require('express-session');
+var passport = require('passport');
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
-var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
+app.use(cookieParser());
+app.use(session({ secret:  'this is the secret' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-require ("./test/app.js")(app);
+//require ("./test/app.js")(app);
+require("./assignment/app.js")(app);
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP   || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 app.listen(port, ipaddress);
+
